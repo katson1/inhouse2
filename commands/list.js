eb02641ec9e120b53b8c7cad627978065a1c0e1e
Katson Alves <katson.alves@ccc.ufcg.edu.br>
2024-05-10 22:43:57 -0300

    list - corirgindo list

--- commands/list.js
+++ commands/list.js
@@ -1,6 +1,13 @@
 import { SlashCommandBuilder } from "discord.js";
 import { getEmbed } from "../utils/embed.js";
 import Player from '../model/playermodel.js';
+import Team1 from '../model/team1model.js';
+import Team2 from '../model/team2model.js';
+
+const team1 = new Team1('mydb.sqlite');
+const team2 = new Team2('mydb.sqlite');
+const teamOne = await team1.getTeam1();
+const teamTwo = await team2.getTeam2();
 
 export default {
     data: new SlashCommandBuilder()
@@ -21,14 +28,17 @@ export default {
             await interaction.reply("Canal não encontrado!");
             return;
         }
+        
+        const members = Array.from(channel.members.values()).map(member => member.user.username);
+        const players = [...teamOne, ...teamTwo].map(team => team.player);
+        const filteredMembers = members.filter(member => !players.includes(member));
 
-        const members = Array.from(channel.members.values()).map(member => member.user.username).join(', ');
         const exampleEmbed = getEmbed();
         exampleEmbed.title = 'Usuários no canal lobby:';
         exampleEmbed.fields.push(
             {
                 name: "Usuários",
-                value: members || "Nenhum usuário no canal",
+                value: filteredMembers.join(', ') || "Nenhum usuário no canal",
                 inline: false,
             }
         );
