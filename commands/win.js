3f10ff7b210f74384d9fe47646f136f7891ada7a
Katson Alves <katson.alves@ccc.ufcg.edu.br>
2024-05-27 18:27:14 -0300

    mmr - updating mmr system

--- commands/win.js
+++ commands/win.js
@@ -25,8 +25,11 @@ export default {
         const winner = interaction.options.getString('winner');
 
         const teamOne = await team1.getTeam1();
-        const teamTwo = await team2.getTeam2();        
-        
+        const teamTwo = await team2.getTeam2();
+        const team1MMR = await team1.getTeam1MMR();
+        const team2MMR = await team2.getTeam2MMR();
+
+
         if (teamOne.length < 5) {
             winEmbed.title = 'Team 1 isn\'t complete!';
             await interaction.reply({ embeds: [winEmbed] });
@@ -41,19 +44,105 @@ export default {
 
         if (winner == "team_1") {
             for (const player of teamOne) {
-                await playerModel.updatePlayerWinStats(player.player);
+                let percentageDifference = ((player.mmr - team2MMR) / player.mmr) * 100;
+                let winMMR = 15;
+                if (percentageDifference < -10) {
+                    winMMR = 16;
+                }
+                if (percentageDifference < -15) {
+                    winMMR = 17;
+                }
+                if (percentageDifference > 15) {
+                    winMMR = 14;
+                }
+                if (player.mmr > 2500) {
+                    winMMR -= 1;
+                }
+                if (player.mmr > 2900) {
+                    winMMR -= 1;
+                }
+                if (player.mmr < 1700) {
+                    winMMR += 1;
+                }
+                if (player.mmr < 1800) {
+                    winMMR += 1;
+                }
+                await playerModel.updatePlayerWinStats(winMMR, player.player);
             }
             for (const player of teamTwo) {
-                await playerModel.updatePlayerLoseStats(player.player);
+                let loseMMR = -15;
+                let percentageDifference = ((player.mmr - team1MMR) / player.mmr) * 100;
+                if (percentageDifference < -10) {
+                    loseMMR = -16;
+                }
+                if (percentageDifference < -15) {
+                    loseMMR = -17;
+                }
+                if (percentageDifference > 15) {
+                    loseMMR = -14;
+                }
+                if (percentageDifference > 20) {
+                    loseMMR = -13;
+                }
+                if (player.mmr > 2500) {
+                    winMMR -= 1;
+                }
+                if (player.mmr > 2900) {
+                    winMMR -= 1;
+                }
+                if (player.mmr < 1700) {
+                    winMMR += 1;
+                }
+                if (player.mmr < 1800) {
+                    winMMR += 1;
+                }
+                await playerModel.updatePlayerLoseStats(loseMMR, player.player);
             }
         }
 
         if (winner == "team_2") {
             for (const player of teamTwo) {
-                await playerModel.updatePlayerWinStats(player.player);
+                let percentageDifference = ((player.mmr - team1MMR) / player.mmr) * 100;
+                let winMMR = 15;
+                if (percentageDifference < -10) {
+                    winMMR = 16;
+                }
+                if (percentageDifference < -15) {
+                    winMMR = 17;
+                }
+                if (percentageDifference > 15) {
+                    winMMR = 14;
+                }
+                if (player.mmr > 2500) {
+                    winMMR -= 1;
+                }
+                if (player.mmr > 2900) {
+                    winMMR -= 1;
+                }
+                await playerModel.updatePlayerWinStats(winMMR, player.player);
             }
             for (const player of teamOne) {
-                await playerModel.updatePlayerLoseStats(player.player);
+                let percentageDifference = ((player.mmr - team2MMR) / player.mmr) * 100;
+                let loseMMR = -15;
+                if (percentageDifference < -10) {
+                    loseMMR = -16;
+                }
+                if (percentageDifference < -15) {
+                    loseMMR = -17;
+                }
+                if (percentageDifference > 15) {
+                    loseMMR = -14;
+                }
+                if (percentageDifference > 20) {
+                    loseMMR = -13;
+                }
+                if (player.mmr > 2500) {
+                    winMMR -= 1;
+                }
+                if (player.mmr > 2900) {
+                    winMMR -= 1;
+                }
+                await playerModel.updatePlayerLoseStats(loseMMR, player.player);
             }
         }
 
