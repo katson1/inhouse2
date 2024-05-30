16cd5fa78bd65da4d77e37b34b03595c2d6cc6d2
Katson Alves <katson.alves@ccc.ufcg.edu.br>
2024-05-29 21:04:12 -0300

    images - adding comments to identify images

--- utils/images.js
+++ utils/images.js
@@ -1,11 +1,11 @@
 export const images = [
-    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/blt9b50b8ceae7a8483/65cacbe7857c8c74b1990f7d/media_gallery_1.webp?",
-    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/blt06cd2eadefbbacf1/65cacbe7492c0f8557bb4062/media_gallery_2.webp?",
-    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/bltb7d581ff87a9230c/65cacbe788b8ab45d859c1c0/media_gallery_3.webp?",
-    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/blt688ca0a519faab47/65cacbe7af7bc95b49c94620/media_gallery_4.webp?",
-    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/blt6935b1ed03ecaab8/65cacbe7b3fe315681b54455/media_gallery_5.webp?",
-    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/bltd5e30354a77232df/65cacbe8653c6888ef99f116/media_gallery_7.webp?",
-    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/blt8ddd357385a0d5cb/65cacbe8d44e9b4cf0e43b5f/media_gallery_8.webp?",
-    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/blt211517a9020bf883/65cacbe8b3fe3182a9b54459/media_gallery_10.webp?",
-    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/blt94d55eea54a186a1/65cacbebe469d005498381ea/media_gallery_12.webp?"
+    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/blt9b50b8ceae7a8483/65cacbe7857c8c74b1990f7d/media_gallery_1.webp?",  //tumb
+    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/blt06cd2eadefbbacf1/65cacbe7492c0f8557bb4062/media_gallery_2.webp?",  //hanamura
+    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/bltb7d581ff87a9230c/65cacbe788b8ab45d859c1c0/media_gallery_3.webp?",  //braxis
+    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/blt688ca0a519faab47/65cacbe7af7bc95b49c94620/media_gallery_4.webp?",  //cursed
+    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/blt6935b1ed03ecaab8/65cacbe7b3fe315681b54455/media_gallery_5.webp?",  //sky
+    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/bltd5e30354a77232df/65cacbe8653c6888ef99f116/media_gallery_7.webp?",  //BoE
+    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/blt8ddd357385a0d5cb/65cacbe8d44e9b4cf0e43b5f/media_gallery_8.webp?",  //infernal
+    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/blt211517a9020bf883/65cacbe8b3fe3182a9b54459/media_gallery_10.webp?", //towers
+    "https://blz-contentstack-images.akamaized.net/v3/assets/blt0e00eb71333df64e/blt94d55eea54a186a1/65cacbebe469d005498381ea/media_gallery_12.webp?"  //dragon
 ];
