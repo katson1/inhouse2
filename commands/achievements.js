22288faadbba600313439dac79b78d9888094b0a
Katson Alves <katson.alves@ccc.ufcg.edu.br>
2024-06-14 04:03:36 -0300

    refact - some changes

--- commands/achievements.js
+++ commands/achievements.js
@@ -22,14 +22,40 @@ export default {
         async execute(interaction) {
             const player = interaction.options.getUser('player');
             const achievementsEmbed = getEmbed();
-            achievementsEmbed.title = '🎯  Achievements';
-            achievementsEmbed.description = 'We are working on it, soon we will also have achievements in the inhouse bot!';
+            achievementsEmbed.color = 0xFF6961;
+            achievementsEmbed.title = '🎯  Achievements:';
+            achievementsEmbed.description = '(beta test* - still not working properly*)';
             if (player) {
                 const globalName = player.globalName;
                 achievementsEmbed.title = `🎯  ${globalName} achievements`;
                 console.log(player);
             } else {
-                
+                achievementsEmbed.fields.push({
+                    name: `<:beata1x1:1248394639850078260> - Beta tester`,
+                    value: `Join Inhouse during the beta test version. \`(0 members)\``,
+                    inline: false
+                },
+                {
+                    name: `<:top1:1248406549018443939> - #1`,
+                    value: `Reach the top of the leaderboard once. \`(0 members)\``,
+                    inline: false
+                },
+                {
+                    name: `💡 - Helper`,
+                    value: `Suggest an improvement for the bot using **/suggest**. \`(0 members)\``,
+                    inline: false
+                },
+                {
+                    name: `Winner:`,
+                    value: `🥉 - Level 1 - Win 10 games or more. \`(0 members)\`\n🥈 - Level 2 - Win 25 games or more. \`(0 members)\`\n🥇 - Level 3 - Win 50 games or more. \`(0 members)\`\n⭐ - Level 4 - Win 100 games or more. \`(0 members)\``,
+                    inline: false
+                },
+                {
+                    name: `Gamer:`,
+                    value: `♟️ - Level 1 - Play 20 games or more. \`(0 members)\`\n🕹️ - Level 2 - Play 50 games or more. \`(0 members)\`\n🎮 - Level 3 - Play 100 games or more. \`(0 members)\``,
+                    inline: false
+                }
+            );
             }
              interaction.reply({ embeds: [achievementsEmbed] } );
         }
